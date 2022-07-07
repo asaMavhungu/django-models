985dd32347be004c3b1c451d59bcb1cf4594b018
asakundwi <mavhasa17@gmail.com>
2022-07-07 23:48:48 +0200

    Added 'flight' template and url

--- flights/models.py
+++ flights/models.py
@@ -6,6 +6,9 @@ class Airport(models.Model):
     code = models.CharField(max_length=3)
     city = models.CharField(max_length=64)
 
+    def __str__(self) -> str:
+        return f"{self.city} ({self.code})"
+
 
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
